4a10aebd92f119e356ac1bc7f023938bc84a000a
tsuna <richardcasinilloph@gmail.com>
2026-05-11 22:45:06 +0800

    config ts

--- ipt-2026-frontend/src/app/app.config.ts
+++ ipt-2026-frontend/src/app/app.config.ts
@@ -1,11 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { appInitializer } from '@app/_helpers';
+import { jwtInterceptor, errorInterceptor } from '@app/_helpers';
+import { AccountService } from '@app/_services';
+import { fakeBackendProvider } from '@app/_helpers';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    providers: [
+        provideRouter(routes),
+        provideHttpClient(withInterceptors([jwtInterceptor, errorInterceptor])),
+        {
+            provide: APP_INITIALIZER,
+            useFactory: appInitializer,
+            deps: [AccountService],
+            multi: true
+        },
+        fakeBackendProvider
+    ]
 };
