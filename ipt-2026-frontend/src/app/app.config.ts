eb6a9265d8af65376577b64b1df0ae86a38419c6
tsuna23222 <richardcasinilloph@gmail.com>
2026-05-18 01:36:52 +0800

    config ts remove fake backend

--- ipt-2026-frontend/src/app/app.config.ts
+++ ipt-2026-frontend/src/app/app.config.ts
@@ -1,21 +1,32 @@
 import { ApplicationConfig, APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { appInitializer } from '@app/_helpers';
 import { jwtInterceptor, errorInterceptor } from '@app/_helpers';
 import { AccountService } from '@app/_services';
+import { FakeBackendInterceptor } from '@app/_helpers';
+import { environment } from '@environments/environment';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
-        provideHttpClient(withInterceptors([jwtInterceptor, errorInterceptor])),
+        provideHttpClient(
+            withInterceptors([jwtInterceptor, errorInterceptor]),
+            withInterceptorsFromDi()
+        ),
         {
             provide: APP_INITIALIZER,
             useFactory: appInitializer,
             deps: [AccountService],
             multi: true
-        }
+        },
+        ...(environment.production ? [] : [{
+            provide: HTTP_INTERCEPTORS,
+            useClass: FakeBackendInterceptor,
+            multi: true
+        }])
     ]
 };
